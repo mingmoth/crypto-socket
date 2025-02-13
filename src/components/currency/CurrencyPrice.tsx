34a9394e598697b8e187c339777c4e95da3c723e
guanmin <guanmin.l@gaszoos.com>
2025-02-13 14:43:12 +0800

    feat: 新增 CurrencyPrice 顯示即時幣別價格

---
+++ src/components/currency/CurrencyPrice.tsx
@@ -0,0 +1,51 @@
+import { useCallback, useRef, useState } from "react";
+import { useCryptoWSChannelSubscribe } from "../../hooks/useCryptoWSSubscribe";
+import { handleCryptoWSTickerChannelMsg } from "../../utils/cryptoMsgHandler";
+import { tickerChannel } from "../../configs/cryptoWSConfig";
+import type { TickerResponse } from "../../types";
+
+interface CurrencyPriceProps {
+  currency: string;
+}
+
+export default function CurrencyPrice({ currency }: CurrencyPriceProps) {
+  const [currPrice, setCurrPrice] = useState(0);
+  const prevPrice = useRef(0);
+
+  // 註冊 candlestick 訊息
+  // @see: https://exchange-docs.crypto.com/exchange/v1/rest-ws/index.html#ticker-instrument_name
+  const channel = `${tickerChannel}.${currency}`;
+
+  const handleCandlestickMsg = useCallback(
+    (data: TickerResponse[]) => {
+      const candleData = handleCryptoWSTickerChannelMsg(data);
+      if (!candleData) return;
+      if (candleData.length === 1) {
+        const data = candleData[0];
+        // 更新最後一筆 candlestick 資料
+        const { price } = data;
+        if(price === 0 || !price) return;
+        setCurrPrice((prev) => {
+          prevPrice.current = prev;
+          return price;
+        });
+      }
+    },
+    [currency]
+  );
+
+  const priceColor = () =>
+    currPrice > prevPrice.current
+      ? "green"
+      : currPrice < prevPrice.current
+      ? "red"
+      : "gray";
+
+  useCryptoWSChannelSubscribe(channel, handleCandlestickMsg);
+
+  return (
+    <h3 className="currency-price" style={{ color: priceColor() }}>
+      {currPrice === 0 ? "-" : currPrice}
+    </h3>
+  );
+}
