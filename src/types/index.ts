162393afda9fa5cdc5f2b1988d18f37a8c2cc3c1
guanmin <guanmin.l@gaszoos.com>
2025-02-13 14:34:26 +0800

    feat: 新增 types 定義

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export interface Candlestick {
+  time: Date;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+}
+
+export interface CandleStickResponse {
+  t: Date;
+  o: number;
+  h: number;
+  l: number;
+  c: number;
+  v: number;
+}
+
+export interface OrderBookData {
+  asks: [number, number][];
+  bids: [number, number][];
+}
+
+export interface TickerResponse {
+  a: number,
+  h: number,
+  l: number,
+  t: number
+}
