df7f75d566d5fbad056cb9d882590b56f8f1728e
guanmin <guanmin.l@gaszoos.com>
2025-02-13 14:31:47 +0800

    feat: 新增 圖表及websocket相關 config

---
+++ src/configs/cryptoWSConfig.ts
@@ -0,0 +1,17 @@
+export const bookInstrumentDepth = 10;
+
+export const candlestickChannel = "candlestick";
+export const candlestickDataSize = 60;
+export const candlestickUrl = "https://api.crypto.com/exchange/v1/public/get-candlestick";
+
+export const cryptoWSHeartbeat = "public/heartbeat";
+export const cryptoWSRespondHeartbeat = "public/respond-heartbeat";
+
+export const orderBookChannel = "book";
+export const orderSize = 5;
+
+export const tradeChannel = "trade";
+export const tickerChannel = "ticker";
+
+export const wsSubscribeMethod = "subscribe";
+export const wsUnSubscribeMethod = "unsubscribe";
