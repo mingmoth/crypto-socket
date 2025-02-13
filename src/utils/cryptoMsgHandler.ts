b99852149029743377d9d20063fa88d5a7db036e
guanmin <guanmin.l@gaszoos.com>
2025-02-13 14:36:25 +0800

    feat: 新增 handler 處理三方來源資料方法

---
+++ src/utils/cryptoMsgHandler.ts
@@ -0,0 +1,40 @@
+import { orderSize } from "../configs/cryptoWSConfig";
+import type { CandleStickResponse, OrderBookData, TickerResponse } from "../types";
+
+
+export function handleCryptoWSBookChannelMsg(data: OrderBookData[]) {
+  if (Array.isArray(data) && data.length > 0) {
+    return {
+      asks: data[0].asks.slice(0, orderSize),
+      bids: data[0].bids.slice(0, orderSize),
+    };
+  }
+  return null
+}
+
+export function handleCryptoWSCandlestickChannelMsg(data: CandleStickResponse[]) {
+  if (Array.isArray(data) && data.length > 0) {
+    return data.map((item) => ({
+      time: new Date(item.t),
+      open: Number(item.o),
+      high: Number(item.h),
+      low: Number(item.l),
+      close: Number(item.c),
+      volume: Number(item.v),
+    }));
+  }
+  return null
+}
+
+export function handleCryptoWSTickerChannelMsg(data: TickerResponse[]) {
+  if (Array.isArray(data) && data.length > 0) {
+    return data.map((tick) => {
+      return {
+        high: Number(tick.h),
+        low: Number(tick.l),
+        price: Number(tick.a),
+        time: new Date(tick.t),
+      }
+    })
+  }
+}
