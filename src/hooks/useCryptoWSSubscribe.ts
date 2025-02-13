f8e409c9cc66114a7f75141f856de4c4db2bf22b
guanmin <guanmin.l@gaszoos.com>
2025-02-13 14:33:47 +0800

    feat: 新增 hook useCryptoWSChannelSubscribe - 各別 websocket channel 訂閱

---
+++ src/hooks/useCryptoWSSubscribe.ts
@@ -0,0 +1,79 @@
+import { useCallback, useEffect } from 'react';
+import { useCryptoWebSocket } from '../contexts/CryptoWSContext';
+import { wsSubscribeMethod, wsUnSubscribeMethod } from "../configs/cryptoWSConfig";
+
+export function useCryptoWSChannelSubscribe<T>(channel: string, handleWSFn: (data: T[]) => void) {
+  if(!channel || typeof handleWSFn !== 'function') {
+    console.error("useCryptoWSChannelSubscribe init error")
+    return;
+  }
+
+  const handleWSChannelMsg = useCallback((event: MessageEvent) => {
+    try {
+      const msg = JSON.parse(event.data)
+      if(msg?.method === wsSubscribeMethod) {
+        if(msg?.result?.channel && msg?.result?.data && msg?.result?.subscription) {
+          const { channel: websocketChannel, subscription, data } = msg?.result;
+          if (channel.startsWith(websocketChannel)
+            && subscription === channel
+            && data
+          ) {
+            return handleWSFn(data);
+          }
+        }
+        
+      }
+      return null;
+    } catch (error) {
+      console.error("Error processing WebScoket Order Book message:", error);
+      return null;
+    }
+  }, [channel])
+
+  const { socket } = useCryptoWebSocket()
+
+  function subscribeWSChannel(channel: string) {
+    if(!socket) return
+    console.log('subscribe', channel)
+    socket.send(JSON.stringify({
+      method: wsSubscribeMethod,
+      params: { channels: channel },
+    }))
+  }
+  
+  function unsubscribeWSChannel(channel: string) {
+    if(!socket) return
+    console.log('unsubscribe', channel)
+    socket.send(JSON.stringify({
+      method: wsUnSubscribeMethod,
+      params: { channels: channel },
+    }))
+  }
+
+  useEffect(() => {
+    if(!socket) return
+    try {
+      if(socket.readyState === WebSocket.OPEN) {
+        subscribeWSChannel(channel)
+      } else {
+        console.warn("Socket is not open. Current readyState:", socket.readyState);
+      }
+    } catch (err) {
+      console.error("Failed to send subscription message:", err);
+    }
+
+    socket.addEventListener("message", handleWSChannelMsg)
+    
+    return (() => {
+      try {
+        if (socket.readyState === WebSocket.OPEN) {
+          unsubscribeWSChannel(channel);
+        }
+      } catch (err) {
+        console.error("Failed to send unsubscribe message:", err);
+      }
+      socket.removeEventListener("message", handleWSChannelMsg)
+
+    })
+  }, [socket])
+}
